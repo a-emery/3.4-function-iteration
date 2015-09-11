00a95060b36ee2dfc606c37e9fa3117eedc1990d
Aaron Emery <aemery14@gmail.com>
2015-09-11 08:59:29 -0400

    Finished up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,3 +25,31 @@ items.filter(function(item) {
   console.log(item.title + " costs $" + item.price);
 }
 });
+
+items.filter(function(item) {
+  if (item.currency_code === "GBP") {
+  console.log(item.title + " costs \u00A3" + item.price);
+}
+});
+
+items.forEach(function (item) {
+  if(item.materials.indexOf('wood') > -1) {
+    console.log(item.title);
+  }
+});
+
+items.filter(function(item) {
+  if (item.materials.length >= 8) {
+  console.log(item.title + " is made of " + item.materials.length + " materials");
+}
+});
+
+var madeBySellers = 0;
+
+items.filter(function(item) {
+  if (item.who_made === 'i_did') {
+  madeBySellers ++;
+}
+});
+
+console.log(madeBySellers);
